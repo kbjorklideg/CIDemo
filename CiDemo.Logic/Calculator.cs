2b632868ca064d749f506b68212268bae042e413
Kalle Björklid <kbjor@eg.fi>
2025-08-27 10:29:48 +0300

    Initial

---
+++ CiDemo.Logic/Calculator.cs
@@ -0,0 +1,6 @@
+﻿namespace CiDemo.Logic;
+
+public class Calculator
+{
+    public int Add(int a, int b) => a + b;
+}
---
+++ CiDemo.Tests/CalculatorTests.cs
@@ -0,0 +1,19 @@
+﻿using CiDemo.Logic;
+
+namespace CiDemo.Tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ShouldReturnCorrectSum()
+    {
+        // Arrange
+        var calculator = new Calculator();
+
+        // Act
+        var result = calculator.Add(2, 3);
+
+        // Assert
+        Assert.Equal(5, result);
+    }
+}
